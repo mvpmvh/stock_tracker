9853959c278b875afa2aa3deb45aebd8ce8fbc7c
Michael Hatch <mvhatch@gmail.com>
2016-06-23 22:18:43 -0500

    configuring mirage settings. created stock model

---
+++ app/models/stock.js
@@ -0,0 +1,12 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+// import { belongsTo, hasMany } from 'ember-data/relationships';
+
+export default Model.extend({
+    name: attr(),
+    symbol: attr(),
+    low: attr(),
+    high: attr(),
+    open: attr(),
+    close: attr()
+});
